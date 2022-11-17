6eeefddbc3d21aaa3eb0236e6f397211c85f1933
jjangjjh2222 <jjangjjh2222@naver.com>
2022-11-18 03:53:25 +0900

    [ADD] 스프링 JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,18 @@
 package hello.hellospring.domain;
 
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public Long getId() {
